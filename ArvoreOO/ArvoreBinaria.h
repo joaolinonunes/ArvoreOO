3c4f7485c10f6d3275086c5f8c80b9976cbe5b57
joaolinonunes <rosana_joao21@hotmail.com>
2020-08-01 20:45:42 -0300

    Arvore Binaria OO

---
+++ ArvoreOO/ArvoreBinaria.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   ArvoreBinaria.h
+ * Author: João
+ *
+ * Created on 1 de Agosto de 2020, 18:20
+ */
+
+#ifndef ARVOREBINARIA_H
+#define	ARVOREBINARIA_H
+
+#include "No.h" //para usar a class No precisa ser incluida
+// A class ArvoreBinaria é a segunda a ser feita pois usaremos a class NO aqui
+class ArvoreBinaria{
+public:
+    ArvoreBinaria();
+    ArvoreBinaria(const ArvoreBinaria& orig);
+    virtual ~ArvoreBinaria();
+    
+    No *raiz;
+
+
+
+};
+
+#endif	/* ARVOREBINARIA_H */
+
--- ArvoreOO/nbproject/Makefile-impl.mk
+++ ArvoreOO/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=arvoreoo
+PROJECTNAME=ArvoreOO
 
 # Active Configuration
 DEFAULTCONF=Debug
