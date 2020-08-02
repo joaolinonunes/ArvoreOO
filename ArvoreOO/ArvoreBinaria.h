7f8fef952a0613b72b635040931cf9ae87d90d90
joaolinonunes <rosana_joao21@hotmail.com>
2020-08-01 22:12:47 -0300

    Arvore Binaria OO

--- ArvoreOO/ArvoreBinaria.h
+++ ArvoreOO/ArvoreBinaria.h
@@ -8,11 +8,21 @@
 #ifndef ARVOREBINARIA_H
 #define	ARVOREBINARIA_H
 
+#include "NO.h" //primeiro passo incluir NO.h
+
 class ArvoreBinaria {
 public:
     ArvoreBinaria();
     ArvoreBinaria(const ArvoreBinaria& orig);
     virtual ~ArvoreBinaria();
+    
+     NO *raiz; //cria um ponteiro em NO
+    void insere(int valor); //metodo inserir valor passando parametro
+    bool vazio(); //metodo para verificar se raiz é nula
+    void PreOrdem(NO *inicio);//metodo pre ordem
+    void emOrdem (NO *inicio);//metodo em ordem
+    void PosOrdem (NO *inicio);//metodo pos ordem
+    
 private:
 
 };
